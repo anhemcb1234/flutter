3fee689bf2ad5117b2f8eea995cbb7b6af96236a
hoangtrungtien99 <hoangtrungtien99@gmail.com>
2022-10-28 17:12:40 +0700

    add

---
+++ lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+final List<String> _items = <String>['A', 'B', 'C', 'D', 'E', 'F'];
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    AppBarExample(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class AppBarExample extends StatefulWidget {
+  const AppBarExample({super.key});
+
+  @override
+  State<AppBarExample> createState() => _AppBarExampleState();
+}
+
+class _AppBarExampleState extends State<AppBarExample> {
+  bool shadowColor = false;
+  double? scrolledUnderElevation;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar Demo'),
+        scrolledUnderElevation: scrolledUnderElevation,
+        shadowColor: shadowColor ? Theme.of(context).colorScheme.shadow : null,
+      ),
+      body: GridView.builder(
+        shrinkWrap: true,
+        itemCount: _items.length,
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 2.0,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Center(
+              child: Text(
+                'Scroll to see the Appbar in effect.',
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+          return Container(
+            alignment: Alignment.center,
+            // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: evenItemColor,
+            ),
+            child: Text('Item $index'),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: OverflowBar(
+            overflowAlignment: OverflowBarAlignment.center,
+            alignment: MainAxisAlignment.center,
+            overflowSpacing: 5.0,
+            children: <Widget>[
+              ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    shadowColor = !shadowColor;
+                  });
+                },
+                icon: Icon(
+                  shadowColor ? Icons.visibility_off : Icons.visibility,
+                ),
+                label: const Text('shadow color'),
+              ),
+              const SizedBox(width: 5),
+              ElevatedButton.icon(
+                onPressed: () {
+                  if (scrolledUnderElevation == null) {
+                    setState(() {
+                      // Default elevation is 3.0, increment by 1.0.
+                      scrolledUnderElevation = 4.0;
+                    });
+                  } else {
+                    setState(() {
+                      scrolledUnderElevation = scrolledUnderElevation! + 1.0;
+                    });
+                  }
+                },
+                icon: const Icon(Icons.add),
+                label: Text(
+                  'scrolledUnderElevation: ${scrolledUnderElevation ?? 'default'}',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
